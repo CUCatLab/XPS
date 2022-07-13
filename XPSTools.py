d8ecffff84f50e900fa803ee52638b98b5d6ae04
Jerry LaRue <33093956+jllarue@users.noreply.github.com>
2022-07-13 15:25:43 -0700

    Converted to class

--- XPSTools.py
+++ XPSTools.py
@@ -40,75 +40,89 @@ pio.templates[pio.templates.default].layout.update(dict(
         )
 ))
 
-folder = os.getcwd()+'/Data'
+##### XPS #####
 
-def Normalize(data) :
-    baseline = np.mean(data[-10:-1])
-    data = data - baseline
-    data = data/max(data)
-    return data
+class XPS () :
 
-def BE(x,Ef) :
-    x -= Ef
-    x = -x
-    return x
+    def __init__(self,folder='') :
 
-def PlotRegion(region) :
+        self.folder = folder
+        self.data = {}
 
-    fig = go.Figure()
-    fig.update_layout(xaxis_title="Energy (eV)",yaxis_title="Intensity (au)",title=region,font=dict(size=18),
-        autosize=False,width=1000,height=600)
+    def Normalize(self,data) :
+        baseline = np.mean(data[-10:-1])
+        data = data - baseline
+        data = data/max(data)
+        return data
 
-    with open('XPS Parameters.yaml', 'r') as stream:
-        par = yaml.safe_load(stream)
-    filenames = list()
-    for filename in par :
-        filenames.append(filename)
+    def BE(self,x,Ef) :
+        x -= Ef
+        x = -x
+        return x
 
-    for filename in filenames :
-        if filename in par and region in par[filename] :
-            data = pd.read_csv(Path(folder+'/'+filename+'_'+str(par[filename][region]['Channel'])+'.csv'))
-            for idx, col in enumerate(data) :
-                if idx == 0 :
-                    x = data[col]
-                    if 'xOffset' in par[filename] :
-                        x = BE(x,par[filename]['xOffset'])
-                elif idx == 1 :
+    def PlotRegion(self,region) :
+
+        data = self.data
+
+        fig = go.Figure()
+        fig.update_layout(xaxis_title="Energy (eV)",yaxis_title="Intensity (au)",title=region,font=dict(size=18),
+            autosize=False,width=1000,height=600)
+
+        with open('XPS Parameters.yaml', 'r') as stream:
+            par = yaml.safe_load(stream)
+        filenames = list()
+        for filename in par :
+            filenames.append(filename)
+
+        for filename in filenames :
+            if filename in par and region in par[filename] :
+                newdata = pd.read_csv(Path(self.folder+'/'+filename+'_'+str(par[filename][region]['Channel'])+'.csv'))
+                data[filename] = {}
+                data[filename][region] = {}
+                for idx, col in enumerate(newdata) :
+                    if idx == 0 :
+                        x = newdata[col]
+                        if 'xOffset' in par[filename] :
+                            x = self.BE(x,par[filename]['xOffset'])
+                    elif idx == 1 :
+                        y = newdata[col]
+                    else :
+                        y += newdata[col]
+                if 'Normalize' in par[filename][region] :
+                    if par[filename][region]['Normalize'] :
+                        y = self.Normalize(y)
+                else: y = self.Normalize(y)
+                if 'yFactor' in par[filename][region] :
+                    y = y/par[filename][region]['yFactor']
+                name = filename
+                if 'Temperature' in par[filename] :
+                    name += ', '+str(par[filename]['Temperature'])+' K'
+                fig.add_trace(go.Scatter(x=x, y=y, name=name))
+                data[filename][region]['x'] = x
+                data[filename][region]['y'] = y
+
+        self.data = data
+        fig.show()
+
+    def PlotFile(self,filename,region,runs=range(0,10000),normalize=False) :
+
+        with open('XPS Parameters.yaml', 'r') as stream:
+            par = yaml.safe_load(stream)
+
+        fig = go.Figure()
+        fig.update_layout(xaxis_title="Energy (eV)",yaxis_title="Intensity (au)",title=filename,font=dict(size=18),
+            autosize=False,width=1000,height=600)
+        data = pd.read_csv(Path(self.folder+'/'+filename+'_'+str(par[filename][region]['Channel'])+'.csv'))
+        for idx, col in enumerate(data) :
+            if idx == 0 :
+                x = data[col]
+                if 'xOffset' in par[filename] :
+                    x = self.BE(x,par[filename]['xOffset'])
+            else :
+                if idx in runs :
                     y = data[col]
-                else :
-                    y += data[col]
-            if 'Normalize' in par[filename][region] :
-                if par[filename][region]['Normalize'] :
-                    y = Normalize(y)
-            else: y = Normalize(y)
-            if 'yFactor' in par[filename][region] :
-                y = y/par[filename][region]['yFactor']
-            name = filename
-            if 'Temperature' in par[filename] :
-                name += ', '+str(par[filename]['Temperature'])+' K'
-            fig.add_trace(go.Scatter(x=x, y=y, name=name))
+                    if normalize :
+                        y = self.Normalize(y)
+                    fig.add_trace(go.Scatter(x=x, y=y, name=idx))
 
-    fig.show()
-
-def PlotFile(filename,region,runs=range(0,10000),normalize=False) :
-
-    with open('XPS Parameters.yaml', 'r') as stream:
-        par = yaml.safe_load(stream)
-
-    fig = go.Figure()
-    fig.update_layout(xaxis_title="Energy (eV)",yaxis_title="Intensity (au)",title=filename,font=dict(size=18),
-        autosize=False,width=1000,height=600)
-    data = pd.read_csv(Path(folder+'/'+filename+'_'+str(par[filename][region]['Channel'])+'.csv'))
-    for idx, col in enumerate(data) :
-        if idx == 0 :
-            x = data[col]
-            if 'xOffset' in par[filename] :
-                x = BE(x,par[filename]['xOffset'])
-        else :
-            if idx in runs :
-                y = data[col]
-                if normalize :
-                    y = Normalize(y)
-                fig.add_trace(go.Scatter(x=x, y=y, name=idx))
-
-    fig.show()
+        fig.show()
